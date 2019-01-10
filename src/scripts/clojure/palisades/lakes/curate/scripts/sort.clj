8abb7044602e6a614b31d1012b637618a87ccb7d
John McDonald <palisades.lakes@gmail.com>
2019-01-10 13:55:26 -0800

    new folders

--- src/scripts/clojure/palisades/lakes/curate/scripts/sort.clj
+++ src/scripts/clojure/palisades/lakes/curate/scripts/sort.clj
@@ -5,7 +5,7 @@
   
   {:doc "rename and de-dupe image files."
    :author "palisades dot lakes at gmail dot com"
-   :version "2019-01-03"}
+   :version "2019-01-10"}
   
   (:require [clojure.java.io :as io]
             [palisades.lakes.curate.curate :as curate]))
@@ -13,26 +13,27 @@
 ;;----------------------------------------------------------------
 ;; TODO: search all drives?
 (with-open [w (io/writer (str "sort.txt"))]
-      (binding [*out* w]
-        (doseq [dir ["iphone6splus-20190102"
-                     #_"iphone6splus-20181203"
-                     #_"nex7-20181203"
-                     #_"nex5-20181129"]]
-          (let [^java.io.File d0 (io/file "e:/" "Pictures" dir)
-                ^java.io.File d1 (io/file "e:/" "pic")]
-            (if (.exists d0)
-              (doseq [^java.io.File f0 (curate/image-file-seq d0)]
-                (println (.getName f0))
-                (curate/rename-image f0 d1))
-              (println "doesn't exist" (.getPath d0)))))))
+  (binding [*out* w]
+    (doseq [dir ["2018-12"
+                 "2019-01"
+                 "iphone6splus-20181203"
+                 #_"nex7-20181203"
+                 #_"nex5-20181129"]]
+      (let [^java.io.File d0 (io/file "e:/" "Pictures" dir)
+            ^java.io.File d1 (io/file "e:/" "pic")]
+        (if (.exists d0)
+          (doseq [^java.io.File f0 (curate/image-file-seq d0)]
+            (println (.getName f0))
+            (curate/rename-image f0 d1))
+          (println "doesn't exist" (.getPath d0)))))))
 #_(with-open [w (io/writer (str "sort.txt"))]
-   (binding [*out* w]
-     (doseq [d ["pic-20181129" "nex5-20181129" "nex7-20181130"]]
-       (let [d0 (io/file "q:/" d)
-             d1 (io/file "e:/" "pic")]
-         (when (.exists d0)
-           (doseq [f0 (curate/image-file-seq d0)]
-             (curate/rename-image f0 d1)))))))
+    (binding [*out* w]
+      (doseq [d ["pic-20181129" "nex5-20181129" "nex7-20181130"]]
+        (let [d0 (io/file "q:/" d)
+              d1 (io/file "e:/" "pic")]
+          (when (.exists d0)
+            (doseq [f0 (curate/image-file-seq d0)]
+              (curate/rename-image f0 d1)))))))
 #_(doseq [drive ["n" 
                  ;;"z" "l" "m"
                  ;;"e" "f" "g" "h" "j" "k" "y"
