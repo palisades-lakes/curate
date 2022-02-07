a2a610f1ba10f4153969de9b07625d89c0d1881e
palisades-lakes <palisades.lakes@gmail.com>
2022-02-07 12:07:41 -0800

    minor

--- src/scripts/clojure/palisades/lakes/curate/scripts/sort.clj
+++ src/scripts/clojure/palisades/lakes/curate/scripts/sort.clj
@@ -5,7 +5,7 @@
   
   {:doc "rename and de-dupe image files."
    :author "palisades dot lakes at gmail dot com"
-   :version "2021-11-16"}
+   :version "2022-02-03"}
   
   (:require [clojure.java.io :as io]
             [palisades.lakes.curate.curate :as curate]))
@@ -14,10 +14,10 @@
 ;;----------------------------------------------------------------
 (with-open [w (io/writer (str "sort.txt"))]
   (binding [*out* w]
-    (doseq [dir ["Pictures"
+    (doseq [dir [#_"Pictures"
+                 #_"photo"
                  "a7c"
                  "a1"
-                 "photo"
                  ]]
       (let [^java.io.File d0 (io/file "z:/"  dir)
             ^java.io.File d1 (io/file "z:/" "sorted")]
