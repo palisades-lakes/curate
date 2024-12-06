fbcd35091437d8e10d90282ec174e36f630a81f0
palisades-lakes <palisades.lakes@gmail.com>
2024-12-06 15:40:50 -0800

    latest dates

--- src/scripts/clojure/palisades/lakes/curate/scripts/sort.clj
+++ src/scripts/clojure/palisades/lakes/curate/scripts/sort.clj
@@ -32,7 +32,7 @@
                               (curate/image-file-seq d0))))
               (println "doesn't exist" (.getPath d0)))))))))
 ;;----------------------------------------------------------------
-(let [tester (curate/after-date? (LocalDate/of 2024 9 24))]
+(let [tester (curate/after-date? (LocalDate/of 2024 10 3))]
 
   (sort-images tester (io/file "X:/" "sorted"))
   (sort-images tester (io/file "Y:/" "selecting")))
