a4160f3efa434179b1e760bfd8bf24e49bc00996
palisades-lakes <palisades.lakes@gmail.com>
2025-02-13 17:05:58 -0800

    latest dates

--- src/scripts/clojure/palisades/lakes/curate/scripts/sort.clj
+++ src/scripts/clojure/palisades/lakes/curate/scripts/sort.clj
@@ -9,11 +9,13 @@
   
   (:require [clojure.java.io :as io]
             [palisades.lakes.curate.curate :as curate])
-  (:import [java.time LocalDate]))
+  (:import (clojure.lang IFn)
+           (java.io File)
+           [java.time LocalDate]))
 
 ;; clj src\scripts\clojure\palisades\lakes\curate\scripts\sort.clj
 ;;----------------------------------------------------------------
-(defn sort-images [^clojure.lang.IFn tester ^java.io.File d1]
+(defn sort-images [^IFn tester ^File d1]
   (let [logfile (io/file d1 "sort.txt")]
     (io/make-parents logfile)
     (with-open [w (io/writer logfile)]
@@ -23,16 +25,16 @@
                      "iphone14"
                      #_"Pictures"
                      #_"portfolio"]]
-          (let [^java.io.File d0 (io/file "Z:/"  dir)]
+          (let [^File d0 (io/file "Z:/" dir)]
             (if (.exists d0)
               (println "new"
                        (reduce
-                         + (map (fn ^long [^java.io.File f0]
+                         + (map (fn ^long [^File f0]
                                 (curate/rename-image f0 tester d1 false))
                               (curate/image-file-seq d0))))
               (println "doesn't exist" (.getPath d0)))))))))
 ;;----------------------------------------------------------------
-(let [tester (curate/after-date? (LocalDate/of 2024 12 4))]
+(let [tester (curate/after-date? (LocalDate/of 2024 1 11))]
 
   (sort-images tester (io/file "X:/" "sorted"))
   (sort-images tester (io/file "Y:/" "selecting")))
